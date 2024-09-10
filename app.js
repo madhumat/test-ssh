10bbe3872902baafa9f4743fe1a56f0e9dff6195
unknown <rudrammategur5@gmail.com>
2024-09-10 11:37:54 +0530

    error handling

--- app.js
+++ app.js
@@ -3,15 +3,36 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const tourRoute =require('./route/tourRoute');
+const globalHandlers = require('./controller/errorController');
 
 console.log(process.env.NODE_ENV);
 // if(process.env.NODE_ENV.trim() === 'dev' ){
 // app.use(morgan('dev'));
 // }
 
+
+
 app.use(express.json()); // To parse JSON request bodies
 app.use('/app/v1/tours', tourRoute);
-app.use(express.static(`${__dirname}`))
+app.use(express.static(`${__dirname}`));
+
+// app.all("*",(req,res,next)=>{
+//     res.status(404).json({
+//       status:"fail",
+//       message:`the error in fetching the requested URL ${req.originalUrl} on the server`
+//     })
+//     next();
+// });
+
+app.all("*",(req,res,next)=>{
+    const err = new Error(`the error in fetching the requested URL ${req.originalUrl} on the server`);
+    err.statusCode =404;
+    err.status = 'fail';
+    next(err);
+});
+
+app.use(globalHandlers);
+
 
 module.exports=app;
 
--- controller/tourController.js
+++ controller/tourController.js
@@ -1,8 +1,7 @@
-
- const { query } = require("express");
+const { query } = require("express");
 const Tour = require("./../model/tourModel"); //JSON.parse(fs.readFileSync(`./tours-simple.json`, 'utf-8'));
 const APIFeatures = require('./../utils/apiFeatures');
-
+const AppError = require('./../utils/AppError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -12,150 +11,59 @@ exports.aliasTopTours = (req, res, next) => {
 };
 
 //functions
-// Get all tours
 exports.getAllTours = async (req, res) => {
-    try {
-        // Fetch all tours from the database
+  // Get all tours
+  try {
+    const features = new APIFeatures(Tour.find(), req.query) // EXECUTE QUERY
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-      //   console.log("get All tours -querry params : ",req.query );
-
-        
-        
-        /** Filtering */
-        // var queryParam = queryString.replace(/\b(gt|gte|lt|lte)\b/g, match=>`$${match}`);
-
-        // console.log(" query replced regex : ", queryParam);
-        //const tours = await Tour.find(JSON.parse(queryParam));
-//----------------------------------------------------------------------------------------------------------------------------------------------
-
-      /**** Sort example
-      const queryString = JSON.stringify(req.query.sort).split(',').join(" ");
-
-      const parsedQuery= JSON.parse(queryString);
-
-      const tours = await Tour.find().sort(parsedQuery); 
-      
-      **/
-
-    // ---------------------------------------------------------------------------------------------------------------------------------------------  
-
-      /******* field example *****
-
-      queryString = JSON.stringify(req.query.fields).split(',').join(" ");
-      const parsedQuery= JSON.parse(queryString);
-      const tours = await Tour.find().select(parsedQuery);**/
-        
-    //----------------------------------------------------------------------------------------------------------------------------------------------
-
-    /**************  Pagination  *************/
-      
-    //  var page = req.query.page*1;
-    //  var limited = req.query.limit*1;
-    //  console.log("page  limited ",page );
-    //  const tours = await Tour.find().skip(page-1).limit(limited);
-        
-
-
-        // Log the tours for debugging purposes
-       // console.log("Tours", req.query);
-
-
-      
-        // EXECUTE QUERY
-        const features = new APIFeatures(Tour.find(), req.query)
-          .filter()
-          .sort()
-          .limitFields()
-          .paginate();
-        const tours = await features.query;
-
-        // Send the successful response with tours data
-        res.status(200).send({
-            message: 'success',
-            status: 200,
-            length: tours.length,
-            data: { tours: tours },
-        });
-    } catch (error) {
-        // Handle any errors that occur during the operation
-        console.error("Error fetching tours:", error);
-
-        // Send an error response
-        res.status(500).send({
-            message: 'An error occurred while fetching tours',
-            status: 500,
-            error: error.message,
-        });
-    }
+    res.status(200).send({
+      // Send the successful response with tours data
+      message: 'success',
+      status: 200,
+      length: tours.length,
+      data: { tours: tours },
+    });
+  } catch (error) {
+    // Handle any errors that occur during the operation
+    next(new AppError('An error occurred while fetching tours', 500));
+  }
+};
+const catchAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch((err) => next(err));
+  };
 };
 
-// Create a new tour
-exports.createNewTour = async (req, res) => {
-    try {
-        // Extract the tour data from the request body
-        //const tourData = req.body;
+exports.createNewTour = catchAsync(async (req, res, next) => {           // Create a new tour
+  const tourData = req.body;                       // Extract the tour data from the request body
+  const newTour = await Tour.create(tourData);    // Create a new tour using the extracted data
+  console.log('New Tour Created', newTour);       // Log the created tour for debugging purposes
+  res.status(201).send({                    // Send a successful response with the created tour data
+    message: 'Tour created successfully',
+    status: 201,
+    data: { tour: newTour },
+  });
+});
 
-        // Create a new tour using the extracted data
-        const newTour = await Tour.create(req.body);
-
-        // Log the created tour for debugging purposes
-        console.log("New Tour Created", newTour);
-
-        // Send a successful response with the created tour data
-        res.status(201).send({
-            message: 'Tour created successfully',
-            status: 201,
-            data: { tour: newTour },
-        });
-    } catch (error) {
-        // Handle any errors that occur during the creation process
-        console.error("Error creating tour:", error);
-
-        // Send an error response
-        res.status(500).send({
-            message: 'An error occurred while creating the tour',
-            status: 500,
-            error: error.message,
-        });
-    }
-};
-
-// Get a specific tour by ID
-exports.findtours = async (req, res) => {
-    try {
-
+exports.findtours =  catchAsync( async (req, res,next) => {     // Get a specific tour by ID
         const tourId =req.params.id;
+        const tours = await Tour.findById(tourId);      // Fetch all tours from the database
 
-        console.log(tourId);
-        // Fetch all tours from the database
-        //const tours = await Tour.find({"_id":tourId});
-        
-        const tours = await Tour.findById(tourId);
-
-
-
-        // Log the tours for debugging purposes
-        console.log("Tours", tours);
-
-        // Send the successful response with tours data
-        res.status(200).send({
+        res.status(200).send({      // Send the successful response with tours data
         message: 'success',
         status: 200,
         length: tours.length,
         data: { tours: tours },
     });
-} catch (error) {
-    // Handle any errors that occur during the operation
-    console.error("Error finding the tour:", error);
-
-    // Send an error response
-    res.status(500).send({
-        message: 'An error occurred while finding tours',
-        status: 500,
-        error: error.message,
-    });
-}
-};
+// } catch (error) {
+//   next(new AppError('An error occurred while fetching Id', 500));
+// }
+});
 // exports.DeleteTour = (req, res) => {
 //   const Id = req.params.id * 1;
 //   const deleteTour = tours.find((el) => el.id === Id);
@@ -167,15 +75,14 @@ exports.findtours = async (req, res) => {
 //   }
 // };
 
-// Update a tour
-exports.updateTour = async (req, res) => {
-  try{
+exports.updateTour = async (req, res) => {    // Update a tour
+  try {
     const tourId = req.params.id;
-  //const tourIndex = tours.findById((el) => el._id === Id);
-    //const tour_id = '66c99bfb180a78977da7fe2e';
-    const updatedTour = await Tour.findByIdAndUpdate(tourId, { name: 'Mangaluru',price:10000, rating:3 }
-                            
-    );
+    const updatedTour = await Tour.findByIdAndUpdate(tourId, {
+      name: 'Mangaluru',
+      price: 10000,
+      rating: 3,
+    });
 
     if (!updatedTour) {
       return res.status(404).json({
@@ -187,15 +94,13 @@ exports.updateTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour: updatedTour
-      }
+        tour: updatedTour,
+      },
     });
-  }catch (error) {
-  // Handle any errors that occur during the operation
-    console.error("Error finding the id of tour:", error);
+  } catch (error) {         // Handle any errors that occur during the operation
+    console.error('Error finding the id of tour:', error);
 
-  // Send an error response
-    res.status(500).send({
+    res.status(500).send({                // Send an error response
       message: 'An error occurred while updating the tours',
       status: 500,
       error: error.message,
@@ -203,11 +108,14 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-
 exports.patchTour = async (req, res) => {
-  try{
+  try {
     const tourId = req.params.id;
-    const patchedTour = await Tour.findByIdAndUpdate(tourId, { name: 'US',price:30000, rating:3 });
+    const patchedTour = await Tour.findByIdAndUpdate(tourId, {
+      name: 'US',
+      price: 30000,
+      rating: 3,
+    });
 
     if (!patchedTour) {
       return res.status(404).json({
@@ -219,24 +127,21 @@ exports.patchTour = async (req, res) => {
     res.status(200).json({
       status: 'success',
       data: {
-        tour: patchedTour
-      }
+        tour: patchedTour,
+      },
     });
-  }catch (error) {
-    // Handle any errors that occur during the operation
-      console.error("Error finding the id of tour:", error);
-  
-    // Send an error response
-      res.status(500).send({
-        message: 'An error occurred while updating the tours',
-        status: 500,
-        error: error.message,
-      });
-    }
-  };
+  } catch (error) {
+    console.error('Error finding the id of tour:', error);
 
-// Delete a tour
-exports.deleteTour = async (req, res) => {
+    res.status(500).send({
+      message: 'An error occurred while updating the tours',
+      status: 500,
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {            // Delete a tour
   try{
     const tourId = req.params.id;
     const deletedTour = await Tour.findOneAndDelete({ _id: tourId });
@@ -255,10 +160,8 @@ exports.deleteTour = async (req, res) => {
       data:null
     });
   }catch (error) {
-    // Handle any errors that occur during the operation
       console.error("Error finding the id of tour:", error);
   
-    // Send an error response
       res.status(500).send({
         message: 'An error occurred while deleting the tours',
         status: 500,
@@ -267,12 +170,11 @@ exports.deleteTour = async (req, res) => {
     }
   };
 
-  //aggregate function
-  exports.getTourStats = async (req, res) => {
+  exports.getTourStats = async (req, res) => {      //aggregate function
     try {
       const stats = await Tour.aggregate([
         {
-          $match: { ratingsAverage: { $gte: 4.5 } }
+          $match: { ratingsAverage: { $gte: 4.5 } },
         },
         {
           $group: {
@@ -282,32 +184,30 @@ exports.deleteTour = async (req, res) => {
             avgRating: { $avg: '$ratingsAverage' },
             avgPrice: { $avg: '$price' },
             minPrice: { $min: '$price' },
-            maxPrice: { $max: '$price' }
-          }
+            maxPrice: { $max: '$price' },
+          },
         },
         {
-          $sort: { avgPrice: 1 }
-        }
+          $sort: { avgPrice: 1 },
+        },
       ]);
-  
+
       res.status(200).json({
         status: 'success',
         data: {
-          stats
-        }
+          stats,
+        },
       });
-    }catch (error) {
-      // Handle any errors that occur during the operation
-        console.error("Error finding the id of tour:", error);
-    
-      // Send an error response
-        res.status(500).send({
-          message: 'An error occurred while deleting the tours',
-          status: 500,
-          error: error.message,
-        });
-      }
-    };
+    } catch (error) {
+      console.error('Error finding the id of tour:', error);
+
+      res.status(500).send({
+        message: 'An error occurred while deleting the tours',
+        status: 500,
+        error: error.message,
+      });
+    }
+  };
 
     exports.getMonthlyPlan = async (req, res) => {
       try {
